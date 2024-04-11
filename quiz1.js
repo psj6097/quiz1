9777d307c6aa6ce4cfc5d596d3eda71a9f1bef35
psj6097 <acrdd7285@naever.com>
2024-04-11 18:52:03 +0900

    quiz1

---
+++ quiz1.js
@@ -0,0 +1,16 @@
+const a = maxvalue([1, 2, 3, 4, 5]);
+console.log(a);
+
+let maxValue;
+let sortArr;
+
+for (let i = a.length - 1; (i) => 0; i--) {
+  if (a[i] > max) {
+    max = a[i];
+  }
+  sortArr.add = a[i];
+}
+
+maxValue = max;
+
+console.log(maxValue, sortArr);
